113975dde07fa43c2195cd30a70d266fea959da9
MaxiEurope <melmaxik@gmail.com>
2024-01-10 16:42:22 +0100

    readfile

---
+++ util/readfile.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char* readfile(const char* filepath) {
+    FILE* file = fopen(filepath, "r");
+    if (file == NULL) {
+        printf("Failed to open the file.\n");
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    char* json_data = (char*)malloc(file_size + 1);
+    fread(json_data, 1, file_size, file);
+    json_data[file_size] = '\0';
+
+    fclose(file);
+    return json_data;
+}
