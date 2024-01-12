9715de98067014cb5c1996585ac3b0cd83a2c40d
MaxiEurope <melmaxik@gmail.com>
2024-01-12 15:18:25 +0100

    recipe header file

--- util/parserecipe.c
+++ util/parserecipe.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "util/ext/cJSON.h"
 #include "util/recipe.h"
-#include "util/readfile.c"
-
-int recipe_count = 0;
-
-/**
- * TODO
- * 
- * put functions into separate files
-*/
 
 Recipe* parserecipe(const char* json_data) {
     cJSON* json = cJSON_Parse(json_data);
@@ -21,7 +11,7 @@ Recipe* parserecipe(const char* json_data) {
         return NULL;
     }
 
-    recipe_count = cJSON_GetArraySize(json);
+    int recipe_count = cJSON_GetArraySize(json);
     printf("Lädt %d Rezepte\n", recipe_count);
     Recipe* recipes = (Recipe*)malloc(recipe_count * sizeof(Recipe));
 
@@ -74,63 +64,4 @@ Recipe* parserecipe(const char* json_data) {
     cJSON_Delete(json);
     
     return recipes;
-}
-
-void freerecipes(Recipe* recipes, int count) {
-    if (recipes == NULL) {
-        return;
-    }
-
-    for (int i = 0; i < count; i++) {
-        free(recipes[i].name);
-        free(recipes[i].instructions);
-
-        for (int j = 0; j < recipes[i].ingredient_count; j++) {
-            free(recipes[i].ingredients[j].name);
-            free(recipes[i].ingredients[j].quantity);
-        }
-        free(recipes[i].ingredients);
-    }
-
-    free(recipes);
-}
-
-int main() {
-    printf("Hello, World!\n");
-    char* json_data = readfile("recipes.json");
-    // printf("%s\n", json_data);
-    if (json_data == NULL) {
-        printf("json_data is null\n");
-        return 1;
-    } else {
-        printf("json_data is not null\n");
-    }
-
-    Recipe* recipes = parserecipe(json_data);
-    if (recipes == NULL) {
-        printf("recipes is null\n");
-        free(json_data);
-        return 1;
-    } else {
-        printf("recipes is not null\n");
-    }
-
-    printf("Lädt %d Rezepte 2\n", recipe_count);
-    for (int i = 0; i < recipe_count; i++) {
-        Recipe* recipe = &recipes[i];
-
-        printf("Recipe %d: %s\n", i + 1, recipe->name);
-        printf("Instructions:\n%s\n", recipe->instructions);
-        printf("Ingredients:\n");
-        for (int j = 0; j < recipe->ingredient_count; j++) {
-            Ingredient* ingredient = &recipe->ingredients[j];
-            printf("  %s %s\n", ingredient->quantity, ingredient->name);
-        }
-        printf("\n");
-    }
-
-    freerecipes(recipes, recipe_count);
-    free(json_data);
-
-    return 0;
 }
