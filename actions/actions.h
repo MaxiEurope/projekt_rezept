b6cc757a78517df2261e57e4f3870a48132733b2
MaxiEurope <melmaxik@gmail.com>
2024-01-19 21:38:38 +0100

    edit recipe

--- actions/actions.h
+++ actions/actions.h
@@ -3,6 +3,7 @@
 
 bool add(int *recipe_count, char *recipe_file);
 bool delete(int *recipe_count, char *recipe_file);
+bool edit(int *recipe_count, char *recipe_file);
 void list(int *recipe_count, char *recipe_file);
 bool search(int *recipe_count, char *recipe_file);
 
